e9e2dda61794694534a662fcecb06d7d52312a62
user <zhehuaxuan@aliyun.com>
2019-09-14 13:27:45 +0800

    webpack支持React和ES6解析

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+
+    /*入口*/
+    entry: path.join(__dirname, '../src/index.js'),
+    mode: 'development',
+
+
+    /*src目录下面的以.js结尾的文件，要使用babel解析*/
+    /*cacheDirectory是用来缓存编译结果，下次编译加速*/
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: ['babel-loader?cacheDirectory=true'],
+            include: path.join(__dirname, '../src')
+        }]
+    },
+
+
+
+    /*输出到dist目录，输出文件名字为bundle.js*/
+    output: {
+        path: path.join(__dirname, '../dist'),
+        filename: 'bundle.js'
+    }
+};
